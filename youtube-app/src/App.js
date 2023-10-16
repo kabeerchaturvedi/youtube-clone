754b069d421b4e301be55e1e9a286e363564db75
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2023-10-16 22:48:08 +0530

    Added components

--- youtube-app/src/App.js
+++ youtube-app/src/App.js
@@ -1,7 +1,29 @@
+import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
 export default function App() {
+  {
+    /* 
+  
+  Header
+  Sidebar 
+   MenuItems
+  Body
+   MainContainer
+    ButtonList
+    VideoContainer
+      VideoCard 
+  
+  
+  */
+  }
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <Header />
+      <Sidebar />
+      <Body />
+    </div>
+  );
 }
