f21f9677b86f8612e1a86959bb3bdaab7e50624f
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2023-11-01 16:24:30 +0530

    Updating Video Container

--- youtube-app/src/components/Buttonlist.js
+++ youtube-app/src/components/Buttonlist.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const Buttonlist = () => {
   return (
-    <div>Buttonlist</div>
-  )
-}
+    <div className="flex">
+      <Button name="Cricket"/>
+      <Button name="Football"/>
+      <Button name="Soccer"/>
+      <Button name="Live"/>
+      <Button name="Music"/>
+      <Button name="Cooking"/>
+      <Button name="BasketBall"/>
+      <Button name="News"/>
+      <Button name="Gaming"/>
+      <Button name="Songs"/>
 
-export default Buttonlist
+    </div>
+  );
+};
+
+export default Buttonlist;
